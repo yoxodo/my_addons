2be6175ba6745789e0a47a3c3a47c0831f687ade
yoxod <registerforme@163.com>
2019-06-16 23:15:30 +0800

    对比功能测试

--- todo/models/models.py
+++ todo/models/models.py
@@ -46,12 +46,41 @@ class TodoTask(models.Model):
 
     @api.multi
     def to_do_compare(self):
-        self.ensure_one()
-        context = self._context
-        # Validation is skipped for brevity
-        selection = context['o2m_selection']
-        # do things with selection data
-        raise ValidationError(context)
+        # self.ensure_one()
+        # context = self._context
+        # # Validation is skipped for brevity
+        # selection = context['o2m_selection']
+        # # do things with selection data
+        # compare_list = self.env['todo.task_detail_compare']
+        # detail_list = self.env['todo.task_detail']
+        # result = compare_list.search([])
+        # for record in result:
+        #     record.unlink()
+        #
+        # for res_id in selection['detail_id']['ids']:
+        #     domain = [('id', '=', res_id)]
+        #     result = detail_list.search(domain, limit=1)
+        #     record = {
+        #         'detail_id': result.id,
+        #         'name': result.name,
+        #         'is_done': result.is_done,
+        #         'deadline': result.deadline,
+        #     }
+        #     compare_list.write(record)
+        result = {
+            'name': 'Detail',
+            'type': 'ir.actions.act_window',
+            'res_model': 'todo.task_detail_compare',
+            'view_mode': 'tree',
+            'view_type': 'tree',
+            'view_id': self.env.ref('todo.todo_task_detail_compare').id,
+            # 'views': [('todo_task_detail_compare', 'tree')],
+            'res_id': False,
+            'context': False,
+            'target': 'current',
+        }
+        return result
+
 
 
 class TaskDetail(models.Model):
@@ -62,3 +91,17 @@ class TaskDetail(models.Model):
     is_done = fields.Boolean('已完成？')
     deadline = fields.Datetime(u'截止时间')
     header_id = fields.Many2one('todo.task', string=u'待办事项')
+
+
+class CompareTaskDetail(models.Model):
+    _name = 'todo.task_detail_compare'
+    _description = '待办事项对比'
+
+    name = fields.Char('明细描述', required=True)
+    is_done = fields.Boolean('已完成？')
+    deadline = fields.Datetime(u'截止时间')
+    detail_id = fields.Integer('明细ID')
+
+    _sql_constraints = [
+        ('detail_id_unique', 'unique (detail_id)', "The values must be unique"),
+    ]
