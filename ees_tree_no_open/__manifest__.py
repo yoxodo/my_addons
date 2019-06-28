c61aa6ae1bd008616784c301fdb1749d175650fe
xudong.yao <xudong.yao@dahuilawyers.com>
2019-06-28 19:22:35 +0800

    删除tree view的final score

---
+++ ees_tree_no_open/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'EESTISOFT tree no open',
+    'version': '12.0.1.0',
+    'author': 'EESTISOFT, ''Giulio Milani, ''Hideki Yamamoto',
+    'category': 'Productivity',
+    'website': 'https://eestisoft.com',
+    'sequence': 2,
+    'summary': 'Allows to set "no open" attribute on tree view',
+    'description': """
+Allows to set "no open" attribute on tree view to prevent interraction upon row click
+	
+Made with love.
+    """,
+	'images':['thumb.png'],
+    'depends': ['base'],
+    'data': ['static/views/view.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
